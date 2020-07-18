7fb4c78c64f41c85941f433ad15b141af7f1878e
Guilherme Paixão <guiferpa@gmail.com>
2020-07-17 23:38:44 -0300

    Improved gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,48 @@
 
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const uglify = require("gulp-uglify")
 
-sass.compiler = require("node-sass"); //Necessário para funcionar gulp-sass
+sass.compiler = require("node-sass"); // It's necessary for gulp-sass works
 
-gulp.task('default', watch);
-
-gulp.task("sass", compilaSass);
-
-function compilaSass() {
+gulp.task('css', () => {
   return gulp
     .src("src/scss/**/*.scss")
-    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError)) // Converte Sass para CSS mimificado com gulp-sass
-    .pipe(gulp.dest("src/css"));
-}
+    .pipe(sass({ outputStyle: "compressed" })) // Convert style to minified file
+    .pipe(gulp.dest('dist'));
+});
 
-function watch() {
-  gulp.watch("src/scss/**/*.scss", compilaSass);
-}
+gulp.task('js', () => {
+  return gulp
+    .src("src/**/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('svg', () => {
+  return gulp
+    .src("src/img/**/*.svg")
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('png', () => {
+  return gulp
+    .src("src/img/**/*.png")
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('jpg', () => {
+  return gulp
+    .src("src/img/**/*.jpg")
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('html', () => {
+  return gulp
+    .src("src/index.html")
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', gulp.series('css', 'js', 'jpg', 'png', 'svg', 'html'));
+
+gulp.task('default', gulp.series('build'));
